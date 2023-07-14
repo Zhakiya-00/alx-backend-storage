a9da5e552f83b1dbfd9498449757a9a9cae219fa
Zhakiya-00 <zakiyayusuf123@gmail.com>
2023-07-15 02:31:03 +0300

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- 2. Best band ever!
-__  ranks country origins of bands, ordered by the number of (non-unique) fans
+-- ranks country origins of bands, ordered by the number of (non-unique) fans
 SELECT origin AS origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
