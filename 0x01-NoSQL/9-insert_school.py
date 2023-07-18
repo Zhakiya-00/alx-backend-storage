12aa9d70cbadc6f5399ec96c92fbcf08de74974f
Zhakiya-00 <zakiyayusuf123@gmail.com>
2023-07-18 23:28:01 +0300

    schools by topic

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,4 +7,4 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
     """ Inserts a new document in a collection based on kwargs """
-    return mongo_collection.insert_one(kwargs).inserted_id
+    return mongo_collection.insert_one(kwargs).inserted_i
